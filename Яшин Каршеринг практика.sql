fccf1f6317962394ea97199d23e448e10c805117
Яшин Роман Владимирович <rom2006bik@gmail.com>
2026-04-22 14:04:38 +0300

    Update Яшин Каршеринг практика.sql

--- Яшин Каршеринг практика.sql
+++ Яшин Каршеринг практика.sql
@@ -9,22 +9,22 @@ SELECT
     b.start_time,
     b.end_time,
     
-    -- Ðàñ÷¸ò äëèòåëüíîñòè â ìèíóòàõ
+
     DATEDIFF(MINUTE, b.start_time, ISNULL(b.end_time, GETDATE())) AS duration_minutes,
     
-    -- Ðàñ÷¸ò ïðîéäåííîãî ðàññòîÿíèÿ (êì)
+
     ISNULL(b.end_mileage, cr.current_mileage_km) - b.start_mileage AS distance_km,
     
-    -- Áàçîâûé òàðèô çà ìèíóòó
+
     t.price_per_minute AS tariff_per_minute,
     
-    -- Òàðèô çà êì (ñâåðõ âêëþ÷åííûõ)
+
     t.price_per_km AS tariff_per_km,
     
-    -- Âêëþ÷åííûå êèëîìåòðû çà âðåìÿ ïîåçäêè
+
     DATEDIFF(HOUR, b.start_time, ISNULL(b.end_time, GETDATE())) * t.included_km_per_hour AS included_km_total,
     
-    -- Ðàñ÷åò ñòîèìîñòè:
+
     ROUND(
         (DATEDIFF(MINUTE, b.start_time, ISNULL(b.end_time, GETDATE())) * t.price_per_minute)
         +
@@ -39,7 +39,7 @@ SELECT
         2
     ) AS calculated_cost,
     
-    -- Äàííûå îá àâòî
+
     m.brand,
     m.model_name,
     cr.license_plate,
